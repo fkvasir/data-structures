70900ccf953367346f1162609193e9454893a056
Fulgent Kvasir Lavesores <90448551+fkvasir@users.noreply.github.com>
2022-10-11 08:50:59 +0800

    added a repository

---
+++ dynamiclinkedlist/circular_linklist(incomplete).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+struct Node
+{
+  int value;
+  struct Node *nextnode;
+};
+
+int main()
+{
+  struct Node *nextnode;
+  struct Node *headnode = new Node;
+
+  if (head == NULL)
+  {
+    return 0;
+  }
+  // pwede rapud ni assert sir
+
+  headnode->value = 1;
+  headnode = NULL;
+
+  Node *tempnode = headnode;
+
+  do
+  {
+    int i = 1;
+    Node *nextnode = new Node;
+    assert(new Node != NULL);
+    ++i;
+    new Node->value = i;
+    new Node->nextnode = headnode;
+    tempnode->nextnode = new Node;
+    tempnode = new Node;
+  } while (int i <= 3);
+  tempnode = headnode;
+
+  return i;
+}
