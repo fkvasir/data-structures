e722ef28aa3832b1696f162269523f8fde17e759
Fulgent Kvasir Lavesores <90448551+fkvasir@users.noreply.github.com>
2023-03-13 12:12:22 +0800

    Update third_max.cpp

--- third max/third_max.cpp
+++ third max/third_max.cpp
@@ -1,28 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+#include <chrono>
 
-int third_Max()
+using namespace std;
+using namespace std::chrono;
+
+int third_Max(vector<int> &nums)
 {
-  float first, second, third;
-  for (int num : nums)
-  {
-    while (i < length(list))
+    long long first = LLONG_MIN, second = LLONG_MIN, third = LLONG_MIN;
+
+    for (int num : nums)
     {
-      if (num > first)
-      {
-        third = second;
-        second = first;
-        first = num;
-      }
-      else if (num > second && num != first)
-      {
-        third = second;
-        second = num;
-      }
-      else if (num > third && num != second)
-      {
-        third = num;
-      }
+        if (num > first)
+            {
+                third = second;
+                second = first;
+                first = num;
+            }
+        else if (num > second && num != first)
+            {
+                third = second;
+                second = num;
+            }
+        else if (num > third && num != second)
+            {
+                third = num;
+            }
     }
-    return third
-  }
+    return third == LLONG_MIN ? first : third;
+}
+
+int main()
+{
+  vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2};
+  int third_max = third_Max(nums);
+  cout << "Third maximum number\n => " << third_max << endl;
+  return 0;
 }
