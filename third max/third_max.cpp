6e4d02b414fd1919ca2b5891891896368c20fc96
Fulgent Kvasir Lavesores <90448551+fkvasir@users.noreply.github.com>
2023-05-15 23:22:29 +0800

    update

--- third max/third_max.cpp
+++ third max/third_max.cpp
@@ -32,10 +32,51 @@ int third_Max(vector<int> &nums)
     return third == LLONG_MIN ? first : third;
 }
 
+void BubbleSort(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = i + 1; j < size; j++)
+    {
+      if (arr[i] > arr[j])
+      {
+            int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+}
+
+int thirdMax(vector<int>& nums) {
+    set<int> maxSet;
+    for (const int& num : nums) {
+        maxSet.insert(num);
+        if (maxSet.size() > 3) {
+            maxSet.erase(maxSet.begin());
+        }
+    }
+    return maxSet.size() == 3 ? *maxSet.begin() : *maxSet.rbegin();
+}
+int ThirdMax2(int arr[], int size)
+{
+    BubbleSort(arr, size);
+    if (size < 3)
+        return arr[0];
+    return arr[size - 3];
+}
+
 int main()
 {
-  vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2};
-  int third_max = third_Max(nums);
-  cout << "Third maximum number\n => " << third_max << endl;
-  return 0;
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2};
+    int thirdmax1= thirdMax1(nums);
+    cout << "thirdMax1:: " << thirdmax1 << endl;
+    int arr_1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2};
+    int len1 = sizeof(arr_1) / sizeof(int);
+    int thirdmax2 = ThirdMax2(arr_1, len1);
+    cout << "ThirdMax2:: " << thirdmax2 << endl;
+    nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2};
+    cout << "thirdMax:: "<<thirdMax(nums) << endl;
+
+    return 0;
 }
